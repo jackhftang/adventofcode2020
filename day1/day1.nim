24be757b7ef7966df6afd19439e668b1c910ac1d
Jack Tang <me@jackhftang.com>
2020-11-30 23:24:32 +0800

    change filename

--- day1/day1.nim
+++ day1/day1.nim
@@ -1,8 +1,14 @@
 import ../common
 
-# const inputFilename = "test.in"
-const inputFilename = "part1.in"
+# const inputFilename = "test.txt"
+const inputFilename = "input.txt"
 const input = staticRead(inputFilename).strip().split("\n").mapIt(parseInt(it))
 
 # ----
 
+proc main() =
+  let a = input
+  
+
+while isMainModule:
+  main()
