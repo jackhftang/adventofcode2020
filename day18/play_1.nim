5876027a12f898883e30b64689a09249571aca01
Jack Tang <me@jackhftang.com>
2020-12-18 18:09:33 +0800

    nicer macros solutions of day18

--- day18/play_1.nim
+++ day18/play_1.nim
@@ -6,10 +6,7 @@ proc `-`(a,b: int): int = a * b
 
 macro eval(s: static[string]): untyped =
   let ns = s.splitLines.mapIt(parseExpr(it.replace("*", "-")))
-  result = newLit(0)
-  for n in ns:
-    result = nnkInfix.newTree(ident"+", result, n)
+  result = ns.foldl(nnkInfix.newTree(ident"+", a, b))
 
-const s = staticRead("day18_input.txt").strip
 static:
-  echo eval(s)
+  echo staticRead("day18_input.txt").strip.eval
--- day18/play_2.nim
+++ day18/play_2.nim
@@ -6,11 +6,11 @@ proc `-`(a,b: int): int = a * b
 proc `*`(a,b: int): int = a + b
 
 macro eval(s: static[string]): untyped =
-  let ns = s.splitLines.mapIt(parseExpr(it.replace("*", "-").replace("+", "*")))
-  result = newLit(0)
-  for n in ns:
-    result = nnkInfix.newTree(ident"+", result, n)
+  let ns = s.splitLines.mapIt(parseExpr it.multiReplace({
+    "+": "*",
+    "*": "-"
+  }))
+  result = ns.foldl(nnkInfix.newTree(ident"+", a, b))
 
-const s = staticRead("day18_input.txt").strip
 static:
-  echo eval(s)
+  echo staticRead("day18_input.txt").strip.eval
