2edcdea6e79134bc279a67f38803007fd3b24b6b
Jack Tang <me@jackhftang.com>
2021-12-01 11:43:00 +0800

    2021

--- day1/day1_1.nim
+++ day1/day1_1.nim
@@ -1,16 +1,10 @@
 import ../common
 
-# const inputFilename = "sample.txt"
-const inputFilename = "input.txt"
-const input = staticRead(inputFilename).strip().split("\n").mapIt(parseInt(it))
+proc main(inputFilename: string) =
+  var input = readFile(currentSourcePath.parentDir / inputFilename).strip.splitLines.map(parseInt)
+  echo input
 
-# ----
 
-proc main() =
-  for i in 0 ..< input.len:
-    for j in i+1 ..< input.len:
-      if input[i] + input[j] == 2020:
-        echo input[i] * input[j]
-  
 when isMainModule:
-  main()
+  # main("day1_input.txt")
+  main("day1_sample1.txt")
