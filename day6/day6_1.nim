4f6dc274641f497ff52a964b5aeff0beaa9ba6e5
Jack Tang <me@jackhftang.com>
2021-12-06 13:21:23 +0800

    added day6

---
+++ day6/day6_1.nim
@@ -0,0 +1,21 @@
+import ../common
+
+proc main(inputFilename: string) =
+  let rawInput = readFile(currentSourcePath.parentDir / inputFilename).strip 
+  var input = rawInput.split(",").map(parseInt)
+  
+  var m = input
+  for i in 1..80:
+    let n = m.len
+    for i in 0 ..< n:
+      if m[i] == 0:
+        m[i] = 6
+        m.add 8
+      else:
+        m[i] -= 1
+  # echo m
+  echo m.len
+
+when isMainModule:
+  # main("day6_sample1.txt")
+  main("day6_input.txt")  
---
+++ day6/day6_2.nim
@@ -0,0 +1,22 @@
+import ../common
+
+proc main(inputFilename: string) =
+  let rawInput = readFile(currentSourcePath.parentDir / inputFilename).strip 
+  var input = rawInput.split(",").map(parseInt).toCountTable
+  
+  var m = input
+  for i in 1..256:
+    let copy = m
+    m.clear()
+    for k, v in copy:
+      var v = copy[k]
+      if k == 0:
+        m.inc(6, v)
+        m.inc(8, v)
+      else:
+        m.inc(k-1, v)
+  echo m.values.toseq.sum
+
+when isMainModule:
+  # main("day6_sample1.txt")
+  main("day6_input.txt")  
