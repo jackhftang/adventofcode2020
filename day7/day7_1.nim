3e4f5e3b7a1eebe5b697a52ee603a46a9c536460
Jack Tang <me@jackhftang.com>
2021-12-07 15:35:11 +0800

    add alternative solution to day7

--- day7/day7_1.nim
+++ day7/day7_1.nim
@@ -3,9 +3,18 @@ import ../common
 proc main(inputFilename: string) =
   let rawInput = readFile(currentSourcePath.parentDir / inputFilename).strip 
   var input = rawInput.split(",").map(parseInt)
-
+  # O(n^2)
   echo arange(0, max(input)).map(y => input.map(x => abs(x-y)).sum).min
+
+proc main2(inputFilename: string) = 
+  let rawInput = readFile(currentSourcePath.parentDir / inputFilename).strip 
+  var input = rawInput.split(",").map(parseInt)
+  # O(n*log(n))
+  let m = input.sorted()[input.len div 2]
+  # O(n)
+  echo input.map(x => abs(x-m)).sum
 
 when isMainModule:
   # main("day7_sample1.txt")
   main("day7_input.txt")  
+  main2("day7_input.txt")  
--- day7/day7_2.nim
+++ day7/day7_2.nim
@@ -3,8 +3,20 @@ import ../common
 proc main(inputFilename: string) =
   let rawInput = readFile(currentSourcePath.parentDir / inputFilename).strip 
   var input = rawInput.split(",").map(parseInt)
+  # O(n^2)
   echo arange(0, max(input)).map(y => input.map(x => abs(x-y)*(abs(x-y)+1) div 2).sum).min
+  
+proc main2(inputFilename: string) =
+  let rawInput = readFile(currentSourcePath.parentDir / inputFilename).strip 
+  var input = rawInput.split(",").map(parseInt)
+
+  let m = input.sum / input.len
+  # O(2*n)
+  let x = input.map(x => (abs(x.float-m), x)).min()[1]
+  # O(2*n)
+  echo input.map(y => abs(x-y)*(abs(x-y)+1) div 2).sum
 
 when isMainModule:
   # main("day7_sample1.txt")
   main("day7_input.txt")  
+  main2("day7_input.txt")  
