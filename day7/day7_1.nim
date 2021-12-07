0ac104b3721837185e42af35169ce2a4721b80be
Jack Tang <me@jackhftang.com>
2021-12-07 13:05:46 +0800

    added day7

---
+++ day7/day7_1.nim
@@ -0,0 +1,11 @@
+import ../common
+
+proc main(inputFilename: string) =
+  let rawInput = readFile(currentSourcePath.parentDir / inputFilename).strip 
+  var input = rawInput.split(",").map(parseInt)
+
+  echo arange(0, max(input)).map(y => input.map(x => abs(x-y)).sum).min
+
+when isMainModule:
+  # main("day7_sample1.txt")
+  main("day7_input.txt")  
