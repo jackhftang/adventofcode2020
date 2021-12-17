58577ec6162b13ca25c9136d9d2ff69b2e876e79
Jack Tang <me@jackhftang.com>
2021-12-17 14:09:28 +0800

    added day17

---
+++ day17/day17_1.nim
@@ -0,0 +1,35 @@
+import moves
+
+proc main(inputFilename: string) =
+  let rawInput = readFile(currentSourcePath.parentDir / inputFilename).strip 
+  var input = rawInput[12..^1].split(",").map(s => s.strip.split("=")[1].split("..").map(parseInt))
+  echo input
+  
+  proc highesty(dp: array[2,int]): int =
+    var v = dp
+    var p = [0,0]
+    var mxy = 0
+    # while p[0] <= input[0][1] and p[1] >= input[1][1]:
+    while true:
+      p = p + v
+      v[0] = max(0, v[0] - 1)
+      v[1] -= 1
+      mxy = max(mxy, p[1])
+      # echo p, mxy
+      if p[0] in input[0][0] .. input[0][1] and p[1] in input[1][0] .. input[1][1]:
+        # echo "in", mxy
+        return mxy
+      elif p[0] > input[0][1] or p[1] < input[1][0]:
+        # echo "out", mxy
+        return int.low
+
+  let mxx = input[0][1]
+  var ans = 0
+  forProd vx, vy in 1 .. mxx, 0 .. 1000:
+    ans = max(ans, highesty([vx, vy]))
+  echo ans
+
+when isMainModule:
+  main("day17_sample_1.txt")
+  main("day17_input.txt")  
+  
---
+++ day17/day17_2.nim
@@ -0,0 +1,42 @@
+import moves
+
+proc main(inputFilename: string) =
+  let rawInput = readFile(currentSourcePath.parentDir / inputFilename).strip 
+  var input = rawInput[12..^1].split(",").map(s => s.strip.split("=")[1].split("..").map(parseInt))
+  echo input
+  
+  proc highesty(dp: array[2,int]): int =
+    var v = dp
+    var p = [0,0]
+    var mxy = 0
+    # while p[0] <= input[0][1] and p[1] >= input[1][1]:
+    while true:
+      p = p + v
+      v[0] = max(0, v[0] - 1)
+      v[1] -= 1
+      mxy = max(mxy, p[1])
+      # echo p, mxy
+      if p[0] in input[0][0] .. input[0][1] and p[1] in input[1][0] .. input[1][1]:
+        # echo "in", mxy
+        return mxy
+      elif p[0] > input[0][1]:
+        # to right area
+        return -1
+      elif p[1] < input[1][0]:
+        return -2
+
+  # echo highesty([7,2])
+  # echo highesty([6,3])
+  # echo highesty([9,0])
+  # echo highesty([17,-4])
+  
+  var cnt = 0
+  forProd vx, vy in 0 .. input[0][1], input[1][0] .. 1000:
+    if highesty([vx, vy]) >= 0:
+      cnt += 1
+  echo cnt 
+
+when isMainModule:
+  main("day17_sample_1.txt")
+  main("day17_input.txt")  
+  
